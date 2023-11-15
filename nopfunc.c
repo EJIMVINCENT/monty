9b423ef109db777f30cc835705ee196f5d9d8595
root <root@EJIMOVIC>
2023-11-15 23:25:52 +0100

    Added descriptions for all functions

--- nopfunc.c
+++ nopfunc.c
@@ -1,10 +1,12 @@
 #include "monty.h"
 
 /**
+ *
  * nopfunc - does nothing
  * @stack: a doubly linked list
  * @montyD: pointer to m
  */
+
 void nopfunc(stack_t **stack, m *montyD)
 {
 	(void)stack;
